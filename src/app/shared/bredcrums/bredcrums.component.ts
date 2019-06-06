c1d3c50c826171454858049001c68039e6ca6bb9
dmg88 <davidmontufar88@gmail.com>
2019-06-06 18:30:55 -0500

    Primer commit

---
+++ src/app/shared/bredcrums/bredcrums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bredcrums',
+  templateUrl: './bredcrums.component.html',
+  styles: []
+})
+export class BredcrumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
